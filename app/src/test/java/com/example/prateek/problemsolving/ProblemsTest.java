800024840182cd6823705255856ad777b045152b
Prateek Kesarwani <prateek@teewe.in>
2017-11-02 13:06:12 +0530

    Initialized ps ds thing

---
+++ app/src/test/java/com/example/prateek/problemsolving/ProblemsTest.java
@@ -0,0 +1,16 @@
+package com.example.prateek.problemsolving;
+
+import org.junit.Test;
+
+/**
+ * Created by prateek on 2/11/17.
+ */
+
+public class ProblemsTest {
+
+    @Test
+    public void firstProblem() {
+        System.out.println("Result");
+    }
+
+}
