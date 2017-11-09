0daa72b151c0cd05847dd92674b75d8d5f0c28c4
Prateek Kesarwani <prateek@teewe.in>
2017-11-09 15:23:12 +0530

    Added multiple test cases

--- app/src/main/java/com/example/prateek/problemsolving/PQueue.java
+++ app/src/main/java/com/example/prateek/problemsolving/PQueue.java
@@ -27,27 +27,47 @@ public class PQueue<T> {
     public void enqueue(T data) {
 
         if (isEmpty()) {
-            front = 0;
             rear = 0;
-        }
-
-        if (isFull()) {
+            front = 0;
+        } else if (isFull()) {
             increaseSize();
+        } else if (front == arr.length - 1) {
+            // If rear is at 0(that means full), this would already be taken care of in isFull above.
+            front = 0;
+        } else {
+            front++;
         }
 
-        arr[front++] = data;
+        arr[front] = data;
     }
 
     private void increaseSize() {
-
+        throw new RuntimeException("Implement increseSize");
     }
 
     public T dequeue() {
-        if (!isEmpty()) {
-            return (T) arr[rear++];
+
+        if (isEmpty()) {
+            return null;
         }
 
-        return null;
+        T data = (T) arr[rear];
+
+        // If only element.
+        if (front == rear) {
+            front = -1;
+            rear = -1;
+        }
+        // If rear is at last element, need to assign it to first, after dequeue.
+        else if (rear == arr.length - 1) {
+            rear = 0;
+        }
+        // Normal case, no edge or only element.
+        else {
+            rear = rear + 1;
+        }
+
+        return data;
     }
 
     public T peek() {
@@ -67,6 +87,14 @@ public class PQueue<T> {
     }
 
     public boolean isFull() {
+
+        if (rear == 0 && front == arr.length - 1) {
+            return true;
+        }
+
+        if (front == rear - 1) {
+            return true;
+        }
 
         return false;
     }
--- app/src/test/java/com/example/prateek/problemsolving/PQueueTest.java
+++ app/src/test/java/com/example/prateek/problemsolving/PQueueTest.java
@@ -2,8 +2,6 @@ package com.example.prateek.problemsolving;
 
 import org.junit.Test;
 
-import java.util.Queue;
-
 import static junit.framework.Assert.assertEquals;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -36,8 +34,121 @@ public class PQueueTest {
     }
 
     @Test
-    public void testIsEmpty() {
+    public void testDequeuePositive() {
         PQueue<String> queue = new PQueue<>();
+
+        queue.enqueue("word1");
+        queue.enqueue("word2");
+        queue.enqueue("word3");
+        queue.enqueue("word4");
+
+        assertEquals("word1", queue.dequeue());
+
+        assertEquals("word2", queue.peek());
+    }
+
+    @Test
+    public void testDequeueNoElement() {
+        PQueue<String> queue = new PQueue<>();
+
+        assertEquals(null, queue.dequeue());
+    }
+
+    @Test
+    public void testDequeueAfterAllDelete() {
+
+        PQueue<String> queue = new PQueue<>();
+
+        queue.enqueue("word1");
+        queue.enqueue("word2");
+
+        queue.dequeue();
+        queue.dequeue();
+
+        assertEquals(null, queue.dequeue());
+        assertEquals(null, queue.peek());
+    }
+
+    @Test
+    public void testDequeueCustom() {
+        PQueue<String> queue = new PQueue<>(5);
+
+        queue.enqueue("word1");
+        queue.enqueue("word2");
+        queue.enqueue("word3");
+        queue.enqueue("word4");
+
+        assertEquals("word1", queue.dequeue());
+        assertEquals("word2", queue.dequeue());
+        assertEquals("word3", queue.dequeue());
+
+        assertEquals("word4", queue.peek());
+
+        queue.enqueue("word5");
+        queue.enqueue("word6");
+        queue.enqueue("word7");
+
+        assertEquals("word4", queue.dequeue());
+        assertEquals("word5", queue.dequeue());
+
+        assertEquals("word6", queue.peek());
+
+        assertEquals("word6", queue.dequeue());
+    }
+
+    @Test
+    public void testIsEmptyNotAdded() {
+        PQueue<String> queue = new PQueue<>();
+        assertEquals(true, queue.isEmpty());
+    }
+
+    @Test
+    public void testIsEmptyAfterAllDeleted() {
+        PQueue<String> queue = new PQueue<>();
+
+        queue.enqueue("word1");
+        queue.enqueue("word2");
+
+        queue.dequeue();
+        queue.dequeue();
+
+        assertEquals(true, queue.isEmpty());
+    }
+
+    @Test
+    public void testIsEmptyFullAndThenDeletedAll() {
+        PQueue<String> queue = new PQueue<>(3);
+
+        queue.enqueue("word1");
+        queue.enqueue("word2");
+        queue.enqueue("word3");
+
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+
+        assertEquals(true, queue.isEmpty());
+    }
+
+    @Test
+    public void testIsEmptyFullThenDeleteSomeThenAddAndDeleteAll() {
+
+        PQueue<String> queue = new PQueue<>(3);
+
+        queue.enqueue("word1");
+        queue.enqueue("word2");
+        queue.enqueue("word3");
+
+        queue.dequeue();
+        queue.dequeue();
+
+        queue.enqueue("word4");
+        queue.enqueue("word5");
+
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+
         assertEquals(true, queue.isEmpty());
     }
 }
